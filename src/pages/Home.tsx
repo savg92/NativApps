ce3e8dd86321468928d84e3b11b0d7221260fc5f
savg92 <1992savg@gmail.com>
2023-10-05 23:43:45 -0300

    updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { getMovies } from '../services/services';
 import { Movie, CartItem } from '../types';
